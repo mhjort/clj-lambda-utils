be86cb2e4f929652dc1ee338103bb6925de97a3a
Markus Hjort <markus.hjort@gmail.com>
2016-03-15 10:03:55 +0200

    Bump aws libraries

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/mhjort/clj-lambda-deploy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[com.amazonaws/aws-java-sdk-lambda "1.10.50"]
-                 [com.amazonaws/aws-java-sdk-core "1.10.50"]
-                 [com.amazonaws/aws-java-sdk-s3 "1.10.50"]]
+  :dependencies [[com.amazonaws/aws-java-sdk-lambda "1.10.60"]
+                 [com.amazonaws/aws-java-sdk-core "1.10.60"]
+                 [com.amazonaws/aws-java-sdk-s3 "1.10.60"]]
   :eval-in-leiningen true)
