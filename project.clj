f8e3061947ef00639d29777e1ae72f23d4fa90c9
Markus Hjort <markus.hjort@gmail.com>
2016-05-13 16:31:33 +0300

    Bump aws s3 libraries

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[cheshire "5.6.1"]
-                 [com.amazonaws/aws-java-sdk-lambda "1.10.60"]
-                 [com.amazonaws/aws-java-sdk-core "1.10.60"]
-                 [com.amazonaws/aws-java-sdk-iam "1.10.60"]
-                 [com.amazonaws/aws-java-sdk-s3 "1.10.60"]]
+                 [com.amazonaws/aws-java-sdk-core "1.10.77"]
+                 [com.amazonaws/aws-java-sdk-lambda "1.10.77"]
+                 [com.amazonaws/aws-java-sdk-iam "1.10.77"]
+                 [com.amazonaws/aws-java-sdk-s3 "1.10.77" :exclusions [com.amazonaws/aws-java-sdk-core]]]
   :eval-in-leiningen true)
