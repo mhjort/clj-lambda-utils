db83fe72ed6211e949e21fd7c58ca9283164697a
Markus Hjort <markus.hjort@gmail.com>
2016-10-05 20:52:51 +0300

    Release Leiningen plugin with 0.2.0 version of library

--- lein-plugin/project.clj
+++ lein-plugin/project.clj
@@ -1,7 +1,7 @@
-(defproject lein-clj-lambda "0.6.1"
+(defproject lein-clj-lambda "0.7.0"
   :description "Leiningen plugin for AWS Lambda deployment"
   :url "https://github.com/mhjort/clj-lambda-deploy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clj-lambda "0.1.0"]]
+  :dependencies [[clj-lambda "0.2.0"]]
   :eval-in-leiningen true)
