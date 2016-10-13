6cc0e45891ecbcbb89fb68faa4a5f13cdb143cd6
Markus Hjort <markus.hjort@gmail.com>
2016-10-13 14:59:11 +0300

    Add support for passing cli flags

--- lein-plugin/src/leiningen/lambda.clj
+++ lein-plugin/src/leiningen/lambda.clj
@@ -3,21 +3,21 @@
             [clj-lambda.aws :as aws])
   (:import [java.io File]))
 
-(defn lambdatask [f project environment]
+(defn lambdatask [f project environment flags]
    (let [deployments (get-in project [:lambda environment])
          jar-file (uberjar project)]
      (when (empty? deployments)
        (throw (ex-info "Could not find anything to install or deploy" {:environment environment})))
-     (f deployments jar-file)))
+     (f deployments jar-file flags)))
 
-(defn update-lambda-task [project environment]
-   (lambdatask aws/update-lambda project environment))
+(defn update-lambda-task [project environment flags]
+   (lambdatask aws/update-lambda project environment flags))
 
-(defn install-lambda-task [project environment]
-  (lambdatask aws/install-lambda project environment))
+(defn install-lambda-task [project environment flags]
+  (lambdatask aws/install-lambda project environment flags))
 
-(defn lambda [project & [task environment]]
+(defn lambda [project & [task environment flag]]
   (condp = task
-    "update" (update-lambda-task project environment)
-    "install" (install-lambda-task project environment)
+    "update" (update-lambda-task project environment [flag])
+    "install" (install-lambda-task project environment [flag])
     (println "Currently only tasks 'update' and 'install' are supported.")))
