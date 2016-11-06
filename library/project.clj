d505268a86269f51b82dcd74b0ccfd37e40e99b8
Jaakko Suutarla <jaakko@suutarla.com>
2016-11-06 11:16:35 +0200

    Update dependencies

--- library/project.clj
+++ library/project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/mhjort/lein-clj-lambda/library"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cheshire "5.5.0"]
-                 [com.fasterxml.jackson.core/jackson-core "2.5.3"]
-                 [com.fasterxml.jackson.core/jackson-databind "2.5.3"]
-                 [com.amazonaws/aws-java-sdk-core "1.10.77" :exclusions [com.fasterxml.jackson.core/jackson-databind]]
-                 [com.amazonaws/aws-java-sdk-lambda "1.10.77"]
-                 [com.amazonaws/aws-java-sdk-iam "1.10.77"]
-                 [com.amazonaws/aws-java-sdk-api-gateway "1.10.77"]
-                 [com.amazonaws/aws-java-sdk-s3 "1.10.77"]])
+  :dependencies [[cheshire "5.6.3"]
+                 [com.fasterxml.jackson.core/jackson-core "2.8.4"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.8.4"]
+                 [com.amazonaws/aws-java-sdk-core "1.11.52" :exclusions [com.fasterxml.jackson.core/jackson-databind]]
+                 [com.amazonaws/aws-java-sdk-lambda "1.11.52"]
+                 [com.amazonaws/aws-java-sdk-iam "1.11.52"]
+                 [com.amazonaws/aws-java-sdk-api-gateway "1.11.52"]
+                 [com.amazonaws/aws-java-sdk-s3 "1.11.52"]])
