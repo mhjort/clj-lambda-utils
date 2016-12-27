4050227ef1e3f3d14f1b0f842e5f91365f4b5a02
Markus Hjort <markus.hjort@gmail.com>
2016-12-27 20:57:28 +0200

    Bump clj-lambda

--- lein-plugin/project.clj
+++ lein-plugin/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-lambda "0.4.0"]
+                 [clj-lambda "0.5.0"]
                  [org.clojure/tools.cli "0.3.5"]]
   :min-lein-version "2.7.1"
   :eval-in-leiningen true)
