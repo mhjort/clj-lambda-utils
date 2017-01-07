410780e6d96eeb35c353da246a63f0cc306f9dc5
Luke Snape <ldjsnape@gmail.com>
2017-01-07 16:16:03 +0000

    Condense both actions into a single boot task

--- boot/src/boot_clj_lambda/boot.clj
+++ boot/src/boot_clj_lambda/boot.clj
@@ -3,30 +3,19 @@
   (:require [boot.core :refer [deftask]]
             [clj-lambda.aws :as aws]))
 
-(deftask install-lambda
-  [l lambda-config    CONFIG  edn  "Lambda configuration keyed by stage name"
-   s stage-name       STAGE   kw   "The environment to install"
-   j jar-file         JARFILE str  "Path to the jar file to upload"
-   o only-api-gateway         bool "Only install API gateway option"]
-  (fn [next-task]
-    (fn [fileset]
-      (aws/install-lambda stage-name
-                          (get lambda-config stage-name)
-                          jar-file
-                          {:only-api-gateway only-api-gateway})
-      (next-task fileset))))
-
-(deftask update-lambda
-  [l lambda-config    CONFIG  edn  "Lambda configuration keyed by stage name"
+(deftask lambda
+  [a action           ACTION  kw   "Action to perform: 'install' or 'update'"
+   l lambda-config    CONFIG  edn  "Lambda configuration keyed by stage name"
    s stage-name       STAGE   kw   "The environment to install"
    j jar-file         JARFILE str  "Path to the jar file to upload"
    o only-api-gateway         bool "Only update API gateway option"]
   (fn [next-task]
     (fn [fileset]
-      (aws/update-lambda stage-name
-                         (get lambda-config stage-name)
-                         jar-file
-                         {:only-api-gateway only-api-gateway})
+      (let [lambda-fn (case action
+                        :install aws/install-lambda
+                        :update aws/update-lambda)]
+        (lambda-fn stage-name
+                   (get lambda-config stage-name)
+                   jar-file
+                   {:only-api-gateway only-api-gateway}))
       (next-task fileset))))
-
-;; boot install-lambda -l "{:foo 1}" -s test -j /tmp/jar.jar -o
