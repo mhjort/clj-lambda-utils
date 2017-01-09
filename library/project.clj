4f029410e8231aa32ec1de08fb33170ef58dc06c
Russell Whitaker <rwhitaker@ulive.com>
2017-01-09 13:26:48 -0800

    Regressed Jackson lib versions

--- library/project.clj
+++ library/project.clj
@@ -5,8 +5,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[cheshire "5.6.3"]
-                 [com.fasterxml.jackson.core/jackson-core "2.8.5"]
-                 [com.fasterxml.jackson.core/jackson-databind "2.8.5"]
+                 [com.fasterxml.jackson.core/jackson-core "2.5.3"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.5.3"]
                  [prismatic/schema "1.1.3"]
                  [com.amazonaws/aws-java-sdk-core ~aws-java-sdk-version
                    :exclusions [com.fasterxml.jackson.core/jackson-databind]]
