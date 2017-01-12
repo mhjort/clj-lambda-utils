1a85c59b3b33f058f6fb04c8526f0143dc3eb78e
Markus Hjort <markus.hjort@gmail.com>
2017-01-12 21:36:42 +0200

    WIP: Example with API GW Ring mapping

---
+++ example/project.clj
@@ -0,0 +1,19 @@
+(defproject example "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-json "0.4.0"]
+                 [compojure "1.4.0"]
+                 [uswitch/lambada "0.1.0"]
+                 [cheshire "5.6.1"]
+                 [ring/ring-core "1.5.1"]]
+  :plugins [[lein-clj-lambda "0.9.1"]]
+  :lambda  {"example" [{:api-gateway {:name "UtilsExampleApi"}
+                        :handler "example.LambdaFn" ;Do not change this
+                        :memory-size 1536 ;Do not change this
+                        :timeout 30 ;Do not change this
+                        :function-name "lambda-utils-example" ;Do not change this
+                        :region "eu-west-1"}]}
+  :aot :all)
