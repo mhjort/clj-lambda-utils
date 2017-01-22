0ddf90af65a7a38eeb69da6e96fe99ad05b59f4a
Markus Hjort <markus.hjort@gmail.com>
2017-01-22 16:55:22 +0200

    WIP stuff

--- example/project.clj
+++ example/project.clj
@@ -4,11 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-core "1.5.1"]
+                 [ring-apigw-lambda-proxy "0.0.2"]
                  [ring/ring-json "0.4.0"]
                  [compojure "1.4.0"]
                  [uswitch/lambada "0.1.0"]
-                 [cheshire "5.6.1"]
-                 [ring/ring-core "1.5.1"]]
+                 [cheshire "5.6.1"]]
   :plugins [[lein-clj-lambda "0.9.1"]]
   :lambda  {"example" [{:api-gateway {:name "UtilsExampleApi"}
                         :handler "example.LambdaFn" ;Do not change this
--- example/src/example/core.clj
+++ example/src/example/core.clj
@@ -3,6 +3,7 @@
             [cheshire.core :refer [parse-stream generate-stream generate-string parse-string]]
             [clojure.java.io :as io]
             [ring.util.codec :as codec]
+            [ring.middleware.apigw :refer [wrap-apigw-lambda-proxy]]
             [ring.middleware.params :refer [wrap-params]]
             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
             [ring.middleware.json :refer [wrap-json-params
@@ -10,41 +11,20 @@
             [ring.util.response :as r]
             [compojure.core :refer :all]))
 
-(defn interpolate-path [params path]
-  (reduce (fn -interpolate-path [acc [key value]]
-            (clojure.string/replace acc
-                         (re-pattern (format "\\{%s\\}" (name key)))
-                         value))
-          path
-          params))
-
 (defroutes app
-  (GET "/v1" {params :params}
+  (GET "/v1/hello" {params :params}
        (let [name (get params :name "World")]
-         (println "params" params)
-         (-> (r/response {:message (format "Hello, %s" name)})))))
+         (-> (rresponse {:message (format "Hello, %s" name)})))))
 
-(def handler (wrap-json-response
-               (wrap-json-params
-                 (wrap-params
-                   (wrap-keyword-params
-                     app)))))
-
-(defn- generate-query-string [params]
-  (clojure.string/join "&" (map (fn [[k v]] (str (name k) "=" v)) params)))
-
-(generate-query-string {:name "a"})
+(def handler (wrap-apigw-lambda-proxy
+               (wrap-json-response
+                 (wrap-json-params
+                   (wrap-params
+                     (wrap-keyword-params
+                       app))))))
 
 (deflambdafn example.LambdaFn [is os ctx]
   (with-open [writer (io/writer os)]
-    (let [payload (parse-stream (io/reader is :encoding "UTF-8") true)
-          request {:uri (:path payload)
-                   :query-string (generate-query-string (:queryStringParameters payload))
-                   :request-method :get}]
-      (println payload)
-      (let [output (handler request)]
-        (println output)
-        (generate-stream {:statusCode (:status output)
-                          :headers (:headers output)
-                          :body (:body output)}
-                         writer)))))
+    (let [payload (parse-stream (io/reader is :encoding "UTF-8") true)]
+      (let [output (handler payload)]
+        (generate-stream output writer)))))
