69e1f53cd3f15936e850de133cdc67cabc233602
Markus Hjort <markus.hjort@gmail.com>
2017-04-02 15:44:35 +0300

    Use bundled AWS Java SDK dependency to avoid conflicting dependencies

--- library/project.clj
+++ library/project.clj
@@ -1,4 +1,3 @@
-(def aws-java-sdk-version "1.11.76")
 (defproject clj-lambda "0.5.2"
   :description "Clojure utilities for AWS Lambda deployment"
   :url "https://github.com/mhjort/lein-clj-lambda/library"
@@ -6,9 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/data.json "0.2.6"]
                  [prismatic/schema "1.1.4"]
-                 [com.amazonaws/aws-java-sdk-core ~aws-java-sdk-version
-                   :exclusions [com.fasterxml.jackson.core/jackson-databind]]
-                 [com.amazonaws/aws-java-sdk-lambda ~aws-java-sdk-version]
-                 [com.amazonaws/aws-java-sdk-iam ~aws-java-sdk-version]
-                 [com.amazonaws/aws-java-sdk-api-gateway ~aws-java-sdk-version]
-                 [com.amazonaws/aws-java-sdk-s3 ~aws-java-sdk-version]])
+                 [com.amazonaws/aws-java-sdk-bundle "1.11.112"]])
