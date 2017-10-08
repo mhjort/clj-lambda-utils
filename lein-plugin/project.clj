aa21c778a2274d4957fd73e7ac1781767db5c301
Markus Hjort <markus.hjort@gmail.com>
2017-10-08 14:41:44 +0300

    Release lein plugin 0.11.0

--- lein-plugin/project.clj
+++ lein-plugin/project.clj
@@ -1,10 +1,10 @@
-(defproject lein-clj-lambda "0.10.3"
+(defproject lein-clj-lambda "0.11.0"
   :description "Leiningen plugin for AWS Lambda deployment"
   :url "https://github.com/mhjort/clj-lambda-deploy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-lambda "0.6.2"]
+                 [clj-lambda "0.7.0"]
                  [org.clojure/tools.cli "0.3.5"]]
   :min-lein-version "2.7.1"
   :eval-in-leiningen true)
