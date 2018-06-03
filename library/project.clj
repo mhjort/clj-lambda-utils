5fd72ba26ffc38a0faaefe579d6f109c850eab69
Markus Hjort <markus.hjort@gmail.com>
2018-06-03 18:38:56 +0300

    Add missing dependency to Clojure

--- library/project.clj
+++ library/project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/mhjort/lein-clj-lambda/library"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/data.json "0.2.6"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.json "0.2.6"]
                  [prismatic/schema "1.1.4"]
                  [com.amazonaws/aws-java-sdk-bundle "1.11.112"]
                  [robert/bruce "0.8.0"]])
