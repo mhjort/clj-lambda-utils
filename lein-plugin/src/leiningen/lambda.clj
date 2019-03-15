ad8e25cddb92a55b60254cf6821406ae7ad0432d
Markus Hjort <markus.hjort@gmail.com>
2019-03-15 14:12:23 +0200

    Add support for uninstall task in Leiningen

--- lein-plugin/src/leiningen/lambda.clj
+++ lein-plugin/src/leiningen/lambda.clj
@@ -3,18 +3,26 @@
             [clojure.tools.cli :refer [parse-opts]]
             [clj-lambda.aws :as aws]))
 
-(defn lambdatask [f project environment flags]
-   (let [config (get-in project [:lambda environment])
-         jar-file (uberjar project)]
-     (when (empty? config)
-       (throw (ex-info "Could not find anything to install or deploy" {:environment environment})))
-     (f environment config jar-file flags)))
+(defn- get-config [project environment]
+  (let [config (get-in project [:lambda environment])]
+    (when (empty? config)
+      (throw (ex-info "Could not find anything to install or deploy" {:environment environment})))
+    config))
+
+(defn- lambdatask [f project environment flags]
+  (let [config (get-config project environment)
+        jar-file (uberjar project)]
+    (f environment config jar-file flags)))
 
 (defn update-lambda-task [project environment flags]
-   (lambdatask aws/update-lambda project environment flags))
+  (lambdatask aws/update-lambda project environment flags))
 
 (defn install-lambda-task [project environment flags]
   (lambdatask aws/install-lambda project environment flags))
+
+(defn uninstall-lambda-task [project environment flags]
+  (let [config (get-config project environment)]
+    (aws/uninstall-lambda environment config flags)))
 
 (def flags
   [["-o" "--only-api-gateway" "Apply only API Gateway changes"]])
@@ -24,4 +32,5 @@
     (condp = task
       "update" (update-lambda-task project environment opts)
       "install" (install-lambda-task project environment opts)
-      (println "Currently only tasks 'update' and 'install' are supported."))))
+      "uninstall" (uninstall-lambda-task project environment opts)
+      (println "Currently only tasks 'update', 'install' and 'uninstall' are supported."))))
